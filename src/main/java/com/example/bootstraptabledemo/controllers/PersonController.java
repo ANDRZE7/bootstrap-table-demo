51baa374cd81f78638f245938a9f589eed5fd835
ANDRZE7 <andrzej.gagola@gmail.com>
2020-05-06 12:38:11 +0200

    DataTable populated with ajax call (Thymeleaf processing removed from template). The data table controls the data on the client side.

--- src/main/java/com/example/bootstraptabledemo/controllers/PersonController.java
+++ src/main/java/com/example/bootstraptabledemo/controllers/PersonController.java
@@ -1,9 +1,11 @@
 package com.example.bootstraptabledemo.controllers;
 
+import com.example.bootstraptabledemo.domain.Person;
 import com.example.bootstraptabledemo.domain.PersonRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class PersonController {
@@ -18,6 +20,12 @@ public class PersonController {
     public String getPersons(Model model) {
         model.addAttribute("persons", personRepository.findAll());
         return "index";
+    }
+
+    @GetMapping({"/api/data"})
+    @ResponseBody
+    public Iterable<Person> getPersons() {
+        return personRepository.findAll();
     }
 
 }

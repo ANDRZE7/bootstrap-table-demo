f720f8c6eaabfa08edb3b0b6e77fa5f254e5a680
ANDRZE7 <andrzej.gagola@gmail.com>
2020-05-08 13:52:56 +0200

    Implementing index page to show tree examples of working with DataTables.

--- src/main/java/com/example/bootstraptabledemo/controllers/PersonController.java
+++ src/main/java/com/example/bootstraptabledemo/controllers/PersonController.java
@@ -1,10 +1,12 @@
 package com.example.bootstraptabledemo.controllers;
 
+import com.example.bootstraptabledemo.domain.Person;
 import com.example.bootstraptabledemo.domain.PersonRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("persons")
